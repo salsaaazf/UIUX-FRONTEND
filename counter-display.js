05e69b76e0d2abcd7ee272452a5152b87d190196
salsa <annisasalsabilaa199@gmail.com>
2025-11-03 19:28:58 +0700

    Update counter-display.js

--- counter-display.js
+++ counter-display.js
@@ -1,10 +1,36 @@
 class CounterDisplay extends HTMLElement {
+    static get observedAttributes() {
+        return ['title', 'value'];
+    }
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+
         this.title = this.getAttribute('title') || 'Interactive Counter';
         this.value = parseInt(this.getAttribute('value')) || 0;
-        
+
+        this.render();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            if (name === 'title') this.title = newValue || 'Interactive Counter';
+            if (name === 'value') this.value = parseInt(newValue) || 0;
+            this.render();
+        }
+    }
+
+    set count(val) {
+        this.value = val;
+        this.shadowRoot.querySelector('.number').textContent = val;
+    }
+
+    get count() {
+        return this.value;
+    }
+
+    render() {
         this.shadowRoot.innerHTML = `
             <style>
                 .display {
@@ -30,15 +56,6 @@ class CounterDisplay extends HTMLElement {
                 <div class="value">Value: <span class="number">${this.value}</span></div>
             </div>
         `;
-    }
-    
-    set count(val) {
-        this.value = val;
-        this.shadowRoot.querySelector('.number').textContent = val;
-    }
-    
-    get count() {
-        return this.value;
     }
 }
 
